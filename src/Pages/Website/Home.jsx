6012f597257e4612cb9cf71667ad7ee564129283
Zarud Vedsar <183065224+zarud-vedsar@users.noreply.github.com>
2025-08-06 10:41:09 +0530

    s

--- src/Pages/Website/Home.jsx
+++ src/Pages/Website/Home.jsx
@@ -84,7 +84,7 @@ function Home() {
                       </defs>
                       <text>
                         <textPath xlinkHref="#circle">
-                          Ghanshyam Group of Institutions, Prayagraj
+                          &nbsp;Ghanshyam Group of Institutions * Prayagraj *
                         </textPath>
                       </text>
                     </svg>
