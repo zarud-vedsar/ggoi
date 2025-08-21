0b13c5c37c387c6653b5eabb73cc5cee85ea2b07
Rajan Sahu <rajan808172@gmail.com>
2025-08-21 11:25:42 +0530

    ggvg

--- src/Pages/Website/UnderGraduation/BachelorOfArts.jsx
+++ src/Pages/Website/UnderGraduation/BachelorOfArts.jsx
@@ -1,13 +1,9 @@
  import React from "react";
 import Footer from '../../../Components/Website/Footer';
 import '../../../Components/Website/assets/css/bachelor-of-arts.css';
-
-import { FaBookOpen } from 'react-icons/fa';
-import { GiScrollQuill } from 'react-icons/gi';
-import { FaFeatherAlt, FaBook, FaTheaterMasks } from 'react-icons/fa';
-import { MdOutlineNewspaper } from 'react-icons/md';
-import { BiTransferAlt } from 'react-icons/bi';
-import { HiOutlineLanguage } from "react-icons/hi2";
+import { FaLanguage, FaGlobeAmericas, FaBalanceScale } from 'react-icons/fa';
+import { GiAncientScroll, GiVote } from 'react-icons/gi';
+import { MdOutlineAdminPanelSettings } from 'react-icons/md';
  
  function BaHSE() {
    return (
@@ -19,33 +15,37 @@ import { HiOutlineLanguage } from "react-icons/hi2";
                     <div className="ba-hse-card1">
                          <div className="card-body">
                             <div className="ba-hse-header">
-                                 <h2 className="mb-3">Bachelor of Arts (B.A.) Program </h2>
-                                <p className="ba-hse-para">The <strong>Bachelor of Arts (B.A.)</strong> at Ghanshyam Group of Institutions is a <strong>three-year 
-                                    undergraduate program</strong> designed to give students a solid foundation in humanities and 
-                                    social sciences. The course nurtures <strong>critical thinking, creativity, communication skills, 
-                                    cultural awareness, and analytical abilities.</strong> </p>
-                                <p className="ba-hse-para">Students can choose specializations such as <strong>Hindi, Sanskrit, and English,</strong> each offering 
-                                unique perspectives and career opportunities. </p>
-                            </div>
-                            <div style={{ display: 'flex', alignItems: 'baseline', gap: '8px' }}>
-                            <FaBookOpen size={28} color="#08568B" />
-                            <h3 className="mb-3">Specializations Offered</h3> 
-                        </div>
-                        <div className="ba-hse-content">
-                           <h4 className="doz-hse-head">B.A. in Hindi </h4>
-                            <h5>Overview: </h5>
-                            <p className="doz-hse-para">The B.A. Hindi program is ideal for students passionate about <strong>Indian literature, 
-                               language, and cultural heritage </strong>. The course explores classical and modern Hindi 
-                               poetry, drama, novels, grammar, and literary criticism. Students also learn how Hindi 
-                               has evolved as the link language of India, connecting diverse communities. </p>
+                                 <h2 className="mb-0">Bachelor of Arts (B.A.)</h2>
+                                  <h4 className="mb-0">Overview:</h4>
+                                <p className="ba-hse-para">The Bachelor of Arts (B.A.) is a <strong>three-year undergraduate program</strong> designed to cultivate 
+                                        broad intellectual and cultural perspectives through the study of humanities and social 
+                                        sciences. At Ghanshyam Group of Institutions, the B.A. program offers a unique balance of 
+                                        <strong>traditional knowledge and contemporary analysis</strong>, preparing students for both academic 
+                                        advancement and diverse professional careers. Students can specialize in<strong> Hindi, Sanskrit, 
+                                        English, History, Geography, Political Science, and Public Administration</strong>, each subject 
+                                        adding depth to their understanding of language, culture, governance, and society. The B.A. 
+                                        program is not just about classroom learning but also about encouraging students to develop 
+                                        a <strong>critical and reflective mindset</strong> that enables them to become responsible citizens and 
+                                        skilled professionals.  </p>
 
-                               <h6 className="ba-hse-head">What You Will Study: </h6>
-                               <p className="doz-hse-text"><span className="pe-2"> <GiScrollQuill size={20} color="#08568B"/></span> Ancient & Medieval Hindi Literature (Bhakti & Riti Kavya) </p>
-                               <p className="doz-hse-text"><span className="pe-2"><FaFeatherAlt size={20} color="#08568B"/></span>Modern Prose, Poetry, and Drama </p>
-                               <p className="doz-hse-text"><span className="pe-2"><MdOutlineNewspaper size={20} color="#08568B"/></span>Hindi Journalism and Mass Communication </p>
-                               <p className="doz-hse-text"><span className="pe-2"><BiTransferAlt size={20} color="#08568B"/></span>Functional Hindi & Translation Studies </p>
-                               <p className="doz-hse-text"><span className="pe-2"><HiOutlineLanguage size={20} color="#08568B"/></span>Linguistics and Grammar </p>
-                        </div>
+                                   <h5 className="mb-1 doz-what-head">Curriculum Highlights / What You Will Study: </h5>
+                                   <p className="doz-hse-para">The program covers a wide range of disciplines that combine cultural appreciation with social awareness. </p>
+
+                                   {/* <h6 className="doz-st-para mb-0"><FaLanguage />Languages (Hindi, English, Sanskrit):</h6> */}
+                                   <p className="doz-hse-text"> Literature (poetry, prose, and drama), linguistics, literary criticism, and creative writing. </p>
+
+                                   {/* <h6 className="doz-st-para"><GiAncientScroll />History:</h6> */}
+                                   <p className="doz-hse-text"> Ancient, Medieval, and Modern Indian history; World history; Indian National Movement; Historiography. </p>
+
+                                     {/* <h6 className="doz-st-para"><FaGlobeAmericas />Geography:</h6> */}
+                                   <p className="doz-hse-text">Physical geography, climatology, economic geography, environmental studies, and regional planning. </p>
+
+                                      {/* <h6 className="doz-st-para"><GiVote />Political Science: </h6> */}
+                                   <p className="doz-hse-text"> Political theories, Indian constitution, governance, comparative politics, international relations, and diplomacy. </p>
+
+                                       {/* <h6 className="doz-st-para"><MdOutlineAdminPanelSettings />Public Administration:</h6> */}
+                                    <p className="doz-hse-text">Principles of administration, Indian administrative systems, public policy, ethics in governance, and development administration. </p>
+                             </div>
                          </div>
                     </div>
                   </div>
